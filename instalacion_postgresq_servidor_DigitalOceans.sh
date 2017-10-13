d998d8b828a732c8930a2ab10bc889ff6ca5d2a5
Jhorkman Bernal <jhorber95@gmail.com>
2017-10-13 15:12:07 -0500

    Comandos instalacion postgresql

---
+++ instalacion_postgresq_servidor_DigitalOceans.sh
@@ -0,0 +1,17 @@
+#! bin/bash
+
+sudo apt-get update
+sudo apt-get install postgresql postgresql-contrib
+apt-cache search postgres
+
+# Basic Server Setup
+# Crear usuario 'posrgres'  y permitir conectarse desde otros servidores
+
+sudo -u postgres psql postgres
+
+# pass: postgres_123@
+
+# crear un usuario 
+sudo -u postgres createuser -D -A -P sir_huila
+
+# pass: 
