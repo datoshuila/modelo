dd83ed3121935a817869a7cd6a3061750af34557
Jhorkman Bernal <jhorber95@gmail.com>
2017-10-13 17:26:15 -0500

    Adición comandos

--- instalacion_postgresq_servidor_DigitalOceans.sh
+++ instalacion_postgresq_servidor_DigitalOceans.sh
@@ -1,5 +1,9 @@
-#! bin/bash
+#!/bin/bash
 
+# *********************************************************************************
+# Comando basicos de PostgreSQL
+# http://www.yolinux.com/TUTORIALS/LinuxTutorialPostgreSQL.html
+# *********************************************************************************
 sudo apt-get update
 sudo apt-get install postgresql postgresql-contrib
 apt-cache search postgres
@@ -15,3 +19,35 @@ sudo -u postgres psql postgres
 sudo -u postgres createuser -D -A -P sir_huila
 
 # pass: 
+
+
+# verificar que el puerto 5432 este habilitado
+
+netstat -nlt
+
+# Agregar ip cliente
+
+sudo nano /etc/postgresql/su_version_postgresql/main/pg_hba.conf
+
+# local      database  user  auth-method  [auth-options]
+# se agregan los datos del host cliente
+#host	all 	all 	192.89.245.245/24  trust
+
+# Ahora debemos permitir que los usuarios se conecte al servidor
+
+sudo nano /etc/postgresql/su_version_postgresql/main/postgresql.conf
+
+# la siguiete linea la editamos
+# listen_addresses = 'localhost'
+# listen_addresses = '*' ==> de esta manera debe quedar, o solo dentro de las comillas escribimos las
+# ips que queremos que se conecten
+
+
+# Finalmente reiniciamos el servidor
+sudo /etc/init.d/postgresql restart 
+
+
+
+# de esta manera ya podemos conectarnos de manera remota al servidor PostgreSQL
+
+
